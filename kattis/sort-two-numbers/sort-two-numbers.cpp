b8b9ec17116d67ef20687661ac5f732723b84864
Dom <dtafoya17@live.com>
2023-03-14 18:54:31 -0600

    added sort two numbers cpp

---
+++ kattis/sort-two-numbers/sort-two-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int nums[2];
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    cin >> nums[0] >> nums[1];
+
+    sort(nums, nums + n);
+
+    for (int i = 0; i < n; ++i)
+        cout << nums[i] << " ";
+
+    return 0;
+}
